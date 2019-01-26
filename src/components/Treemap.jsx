02201bd45de0ed392d17916c857ce3261153ab23
Przemysław Ryciuk <p@ryciuk.com>
2019-01-27 00:19:21 +0100

    Allow specifying paddings as a prop, fix a bug with bottom padding using x scaling instead of y

--- src/components/Treemap.jsx
+++ src/components/Treemap.jsx
@@ -25,41 +25,39 @@ function calculatePos(node) {
   };
 }
 
-export default function Treemap({ root, width, height, labelGetter }) {
+export default function Treemap({ root, width, height, padding, labelGetter }) {
   root
     .sum(d => d.value)
     .sort((a, b) => b.height - a.height || b.value - a.value);
 
   treemap().size([width, height])(root);
 
-  const paddingTop = 20;
-
   root.descendants().forEach((relativeRoot) => {
     relativeRoot.descendants().slice(1).forEach((desc) => {
       let referencePoint;
       let scale;
 
-      // Vertical to bottom.
-      scale = 1 - paddingTop / (relativeRoot.y1 - relativeRoot.y0);
+      // Scale vertically to bottom.
+      scale = 1 - padding[0] / (relativeRoot.y1 - relativeRoot.y0);
       referencePoint = relativeRoot.y1;
       desc.y0 = referencePoint + scale * (desc.y0 - referencePoint);
       desc.y1 = referencePoint + scale * (desc.y1 - referencePoint);
 
-      // Padding scale
-      scale = 1 - 2 / (relativeRoot.x1 - relativeRoot.x0);
-
-      // Vertical to top.
+      // Scale vertically to top.
+      scale = 1 - padding[2] / (relativeRoot.y1 - relativeRoot.y0);
       referencePoint = relativeRoot.y0;
       desc.y0 = referencePoint + scale * (desc.y0 - referencePoint);
       desc.y1 = referencePoint + scale * (desc.y1 - referencePoint);
 
-      // Horizontal to right.
-      referencePoint = relativeRoot.x1;
+      // Scale horizontally to left.
+      scale = 1 - padding[1] / (relativeRoot.x1 - relativeRoot.x0);
+      referencePoint = relativeRoot.x0;
       desc.x0 = referencePoint + scale * (desc.x0 - referencePoint);
       desc.x1 = referencePoint + scale * (desc.x1 - referencePoint);
 
-      // Horizontal to left.
-      referencePoint = relativeRoot.x0;
+      // Scale horizontally to right.
+      scale = 1 - padding[3] / (relativeRoot.x1 - relativeRoot.x0);
+      referencePoint = relativeRoot.x1;
       desc.x0 = referencePoint + scale * (desc.x0 - referencePoint);
       desc.x1 = referencePoint + scale * (desc.x1 - referencePoint);
     });
@@ -84,8 +82,8 @@ export default function Treemap({ root, width, height, labelGetter }) {
                 <div className="treemap__node-inner">
                   <div className="treemap__node-inner2">
                     <div style={{
-                      height: `${paddingTop}px`,
-                      lineHeight: `${paddingTop - 2}px`,
+                      height: `${padding[0]}px`,
+                      lineHeight: `${padding[0] - 2}px`,
                       backgroundColor: item.children ? 'rgba(255,255,255,0.3)' : null,
                       fontSize: '12px',
                       paddingLeft: '4px',
@@ -107,9 +105,11 @@ Treemap.propTypes = {
   root: PropTypes.object.isRequired, // eslint-disable-line react/forbid-prop-types
   width: PropTypes.number.isRequired,
   height: PropTypes.number.isRequired,
+  padding: PropTypes.arrayOf(PropTypes.number),
   labelGetter: PropTypes.func,
 };
 
 Treemap.defaultProps = {
   labelGetter: node => node.data.key,
+  padding: [20, 2, 2, 2],
 };
