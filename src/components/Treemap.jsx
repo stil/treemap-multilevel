ce2df797c0b62002b11b031b0480874f8f252045
Przemysław Ryciuk <p@ryciuk.com>
2019-01-27 01:20:53 +0100

    Make Treemap component fully abstract

--- src/components/Treemap.jsx
+++ src/components/Treemap.jsx
@@ -1,11 +1,7 @@
 /* eslint-disable no-param-reassign */
-import React from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types'; // eslint-disable-line import/no-extraneous-dependencies
 import { treemap } from 'd3-hierarchy';
-import chroma from 'chroma-js';
-import { scaleOrdinal } from 'd3-scale';
-import { schemeCategory10 } from 'd3-scale-chromatic';
-import './Treemap.css';
 
 function canDisplay(node) {
   const width = node.x1 - node.x0;
@@ -25,7 +21,7 @@ function calculatePos(node) {
   };
 }
 
-export default function Treemap({ root, width, height, padding, labelGetter }) {
+export default function Treemap({ root, width, height, padding, nodeComponent }) {
   root
     .sum(d => d.value)
     .sort((a, b) => b.height - a.height || b.value - a.value);
@@ -34,71 +30,42 @@ export default function Treemap({ root, width, height, padding, labelGetter }) {
 
   root.descendants().forEach((relativeRoot) => {
     relativeRoot.descendants().slice(1).forEach((desc) => {
-      let referencePoint;
+      let refPoint;
       let scale;
 
       // Scale vertically to bottom.
       scale = 1 - padding[0] / (relativeRoot.y1 - relativeRoot.y0);
-      referencePoint = relativeRoot.y1;
-      desc.y0 = referencePoint + scale * (desc.y0 - referencePoint);
-      desc.y1 = referencePoint + scale * (desc.y1 - referencePoint);
+      refPoint = relativeRoot.y1;
+      desc.y0 = refPoint + scale * (desc.y0 - refPoint);
+      desc.y1 = refPoint + scale * (desc.y1 - refPoint);
 
       // Scale vertically to top.
       scale = 1 - padding[2] / (relativeRoot.y1 - relativeRoot.y0);
-      referencePoint = relativeRoot.y0;
-      desc.y0 = referencePoint + scale * (desc.y0 - referencePoint);
-      desc.y1 = referencePoint + scale * (desc.y1 - referencePoint);
+      refPoint = relativeRoot.y0;
+      desc.y0 = refPoint + scale * (desc.y0 - refPoint);
+      desc.y1 = refPoint + scale * (desc.y1 - refPoint);
 
       // Scale horizontally to left.
       scale = 1 - padding[1] / (relativeRoot.x1 - relativeRoot.x0);
-      referencePoint = relativeRoot.x0;
-      desc.x0 = referencePoint + scale * (desc.x0 - referencePoint);
-      desc.x1 = referencePoint + scale * (desc.x1 - referencePoint);
+      refPoint = relativeRoot.x0;
+      desc.x0 = refPoint + scale * (desc.x0 - refPoint);
+      desc.x1 = refPoint + scale * (desc.x1 - refPoint);
 
       // Scale horizontally to right.
       scale = 1 - padding[3] / (relativeRoot.x1 - relativeRoot.x0);
-      referencePoint = relativeRoot.x1;
-      desc.x0 = referencePoint + scale * (desc.x0 - referencePoint);
-      desc.x1 = referencePoint + scale * (desc.x1 - referencePoint);
+      refPoint = relativeRoot.x1;
+      desc.x0 = refPoint + scale * (desc.x0 - refPoint);
+      desc.x1 = refPoint + scale * (desc.x1 - refPoint);
     });
   });
 
-  const colorScale = scaleOrdinal(schemeCategory10);
-
-  return (
-    <div>
-      <div style={{ width: `${width}px`, height: `${height}px`, margin: 'auto', overflow: 'hidden' }}>
-        {root.descendants()
-            .filter(item => canDisplay(item))
-            .map((item, i) => (
-              <div
-                key={item.data.key + item.depth}
-                className="treemap__node"
-                style={{
-              ...calculatePos(item, root),
-              background: chroma(colorScale(i)).desaturate().brighten().hex(),
-            }}
-              >
-                <div className="treemap__node-inner">
-                  <div className="treemap__node-inner2">
-                    <div style={{
-                      height: `${padding[0]}px`,
-                      lineHeight: `${padding[0] - 2}px`,
-                      backgroundColor: item.children ? 'rgba(255,255,255,0.3)' : null,
-                      fontSize: '12px',
-                      paddingLeft: '4px',
-                      color: chroma(colorScale(i)).darken(2).hex(),
-                   }}
-                    >
-                      {labelGetter(item)}
-                    </div>
-                  </div>
-                </div>
-              </div>
-            ))}
-      </div>
-    </div>
-  );
+  return root.descendants()
+    .filter(node => canDisplay(node))
+    .map((node, i) => (
+      <Fragment key={node.data.key + node.depth}>
+        {nodeComponent(node, i, calculatePos(node, root))}
+      </Fragment>
+    ));
 }
 
 Treemap.propTypes = {
@@ -106,10 +73,10 @@ Treemap.propTypes = {
   width: PropTypes.number.isRequired,
   height: PropTypes.number.isRequired,
   padding: PropTypes.arrayOf(PropTypes.number),
-  labelGetter: PropTypes.func,
+  nodeComponent: PropTypes.func,
 };
 
 Treemap.defaultProps = {
-  labelGetter: node => node.data.key,
+  nodeComponent: (node, i, posStyle) => <div style={{ ...posStyle, position: 'absolute', background: 'rgba(255,0,0,0.1)' }}>{node.data.key}</div>,
   padding: [20, 2, 2, 2],
 };
--- src/demo/App.jsx
+++ src/demo/App.jsx
@@ -1,7 +1,11 @@
 /* eslint-disable no-console */
-import React, { Fragment } from 'react';
+import React from 'react';
+import chroma from 'chroma-js';
+import { scaleOrdinal } from 'd3-scale';
+import { schemeCategory10 } from 'd3-scale-chromatic';
 import Treemap from '../components/Treemap';
 import countryTree from './data/country-tree';
+import './Treemap.css';
 
 function nFormatter(num, digits) {
   const si = [
@@ -27,17 +31,50 @@ export default class App extends React.Component {
   }
 
   render() {
+    const colorScale = scaleOrdinal(schemeCategory10);
+    const padding = [20, 2, 2, 2];
+    const size = [800, 800];
+
     return (
       <div style={{ fontFamily: 'Roboto' }}>
         <style>
           {"@import url('https://fonts.googleapis.com/css?family=Roboto');"}
         </style>
-        <Treemap
-          root={countryTree}
-          width={800}
-          height={800}
-          labelGetter={n => <Fragment>{n.data.key} ({nFormatter(n.value)})</Fragment>}
-        />
+
+
+        <div style={{ width: `${size[0]}px`, height: `${size[1]}px`, margin: 'auto', overflow: 'hidden' }}>
+          <Treemap
+            root={countryTree}
+            width={size[0]}
+            height={size[1]}
+            padding={padding}
+            nodeComponent={(node, i, posStyle) => (
+              <div
+                className="treemap__node"
+                style={{
+                  ...posStyle,
+                  background: chroma(colorScale(i)).desaturate().brighten().hex(),
+                }}
+              >
+                <div className="treemap__node-inner">
+                  <div className="treemap__node-inner2">
+                    <div style={{
+                      height: `${padding[0]}px`,
+                      lineHeight: `${padding[0] - 2}px`,
+                      backgroundColor: node.children ? 'rgba(255,255,255,0.3)' : null,
+                      fontSize: '12px',
+                      paddingLeft: '4px',
+                      color: chroma(colorScale(i)).darken(2).hex(),
+                   }}
+                    >
+                      {node.data.key} ({nFormatter(node.value)})
+                    </div>
+                  </div>
+                </div>
+              </div>
+          )}
+          />
+        </div>
       </div>
     );
   }
